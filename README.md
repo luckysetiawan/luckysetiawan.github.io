74213f56a3b5e9fccabe2c8c25e6fd2f88616609
luckysetiawan <luckysetiawan0802@gmail.com>
2023-12-21 18:10:55 +0700

    updated contact link from shields.io badges to fontawesome icons

--- README.md
+++ README.md
@@ -9,6 +9,5 @@ This is my personal website. You can visit my website by clicking the link below
 * [reveal.js](https://revealjs.com/)
 * [reveal.js-menu](https://github.com/denehyg/reveal.js-menu)
 * [Google Fonts](https://fonts.google.com/)
-* [Shields.io](https://shields.io/badges)
 * [Font Awesome](https://fontawesome.com/)
 * [particles.js](https://vincentgarreau.com/particles.js/)
--- js/component.js
+++ js/component.js
@@ -12,21 +12,29 @@ class ContactComponent extends HTMLElement {
 
         if (isMobile()) {
             contact = `
-                <a target="_blank" href="https://www.linkedin.com/in/luckysetiawan/"><img class="mobile-contact" src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a><br>
-				<a target="_blank" href="mailto:luckysetiawan0802@gmail.com"><img class="mobile-contact" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"></a><br>
-                <a target="_blank" href="https://github.com/luckysetiawan"><img class="mobile-contact" src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"></a><br>
-                <a target="_blank" href="https://gitlab.com/luckysetiawan"><img class="mobile-contact" src="https://img.shields.io/badge/GitLab-330F63?style=for-the-badge&logo=gitlab&logoColor=white"></a><br>
-                <a target="_blank" href="https://www.youtube.com/@lucky_setiawan"><img class="mobile-contact" src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white"></a><br>
-                <a target="_blank" href="https://www.instagram.com/luckysetia77/"><img class="mobile-contact" src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white"></a>
+                <table>
+                    <tr>
+                        <td><a target="_blank" style="font-size:30vw;" href="https://www.linkedin.com/in/luckysetiawan/"><i class="fa-brands fa-linkedin"></i></a></td>
+                        <td><a target="_blank" style="font-size:30vw;" href="mailto:luckysetiawan0802@gmail.com"><i class="fa-brands fa-google"></i></a></td>
+                    </tr>
+                    <tr>
+                        <td><a target="_blank" style="font-size:30vw;" href="https://github.com/luckysetiawan"><i class="fa-brands fa-github"></i></a></td>
+                        <td><a target="_blank" style="font-size:30vw;" href="https://gitlab.com/luckysetiawan"><i class="fa-brands fa-gitlab"></i></a></td>
+                    </tr>
+                    <tr>
+                        <td><a target="_blank" style="font-size:30vw;" href="https://www.youtube.com/@lucky_setiawan"><i class="fa-brands fa-youtube"></i></a></td>
+                        <td><a target="_blank" style="font-size:30vw;" href="https://www.instagram.com/luckysetia77/"><i class="fa-brands fa-instagram"></i></a></td>
+                    </tr>
+                </table>
             `;
         } else {
             contact = `
-                <a target="_blank" href="https://www.linkedin.com/in/luckysetiawan/"><img class="desktop-contact" src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a>
-				<a target="_blank" href="mailto:luckysetiawan0802@gmail.com"><img class="desktop-contact" src="https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white"></a>
-                <a target="_blank" href="https://github.com/luckysetiawan"><img class="desktop-contact" src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"></a>
-                <a target="_blank" href="https://gitlab.com/luckysetiawan"><img class="desktop-contact" src="https://img.shields.io/badge/GitLab-330F63?style=for-the-badge&logo=gitlab&logoColor=white"></a>
-                <a target="_blank" href="https://www.youtube.com/@lucky_setiawan"><img class="desktop-contact" src="https://img.shields.io/badge/YouTube-FF0000?style=for-the-badge&logo=youtube&logoColor=white"></a>
-                <a target="_blank" href="https://www.instagram.com/luckysetia77/"><img class="desktop-contact" src="https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white"></a>
+                <a target="_blank" style="font-size:5vw;" href="https://www.linkedin.com/in/luckysetiawan/"><i class="fa-brands fa-linkedin"></i></a>
+				<a target="_blank" style="font-size:5vw;" href="mailto:luckysetiawan0802@gmail.com"><i class="fa-brands fa-google"></i></a>
+                <a target="_blank" style="font-size:5vw;" href="https://github.com/luckysetiawan"><i class="fa-brands fa-github"></i></a>
+                <a target="_blank" style="font-size:5vw;" href="https://gitlab.com/luckysetiawan"><i class="fa-brands fa-gitlab"></i></a>
+                <a target="_blank" style="font-size:5vw;" href="https://www.youtube.com/@lucky_setiawan"><i class="fa-brands fa-youtube"></i></a>
+                <a target="_blank" style="font-size:5vw;" href="https://www.instagram.com/luckysetia77/"><i class="fa-brands fa-instagram"></i></a>
             `;
         }
 
